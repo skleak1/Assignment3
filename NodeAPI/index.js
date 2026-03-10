14514bdfbea2e35745acda3096788f20427209ab
SunKhingleak <2024028sun@aupp.edu.kh>
2026-03-10 17:13:21 +0700

    added more api endpoints

--- NodeAPI/index.js
+++ NodeAPI/index.js
@@ -3,13 +3,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Get
 app.get('/', (req, res) => {
   res.send('Welcome to Express API');
 });
 
 app.get('/search', (req, res) => {
   res.send('Inside Searching API');
+});
+
+app.post('/add', (req, res) => {
+  res.send('Inside Adding API');
+});
+
+app.put('/update', (req, res) => {
+  res.send('Inside Updating API');
+});
+
+app.delete('/delete', (req, res) => {
+  res.send('Inside Deleting API');
 });
 
 app.listen(port, () => {
